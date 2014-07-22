20ac7e9e4a4504d093117246f430b849cd16e7b1
Keiran Raine <keiranmraine@gmail.com>
2014-07-22 13:54:06 +0100

    Now builds segments into VCF as part of process

--- perl/bin/CN_to_VCF.pl
+++ perl/bin/CN_to_VCF.pl
@@ -36,6 +36,8 @@ use Pod::Usage qw(pod2usage);
 
 use Bio::DB::Sam;
 use Try::Tiny;
+use PCAP::Cli;
+
 
 use Sanger::CGP::Vcf;
 use Sanger::CGP::Vcf::VCFCNConverter;
@@ -51,12 +53,15 @@ use Sanger::CGP::Vcf::VcfProcessLog;
 
   my $mt_sam = Bio::DB::Sam->new(-bam => $opts->{'sbm'}, -fasta => $opts->{'r'});
   my $wt_sam = Bio::DB::Sam->new(-bam => $opts->{'sbw'}, -fasta => $opts->{'r'});
-  my $fai = Bio::DB::Sam::Fai->load($opts->{'r'});
 
   #parse samples and contigs from the bam files.
   my $contigs = Sanger::CGP::Vcf::BamUtil->parse_contigs($mt_sam->header->text.$wt_sam->header->text,$opts->{'rs'},$opts->{'ra'});
-  my $mt_samples = Sanger::CGP::Vcf::BamUtil->parse_samples($mt_sam->header->text,$opts->{'mss'},$opts->{'msq'},$opts->{'msa'},$opts->{'msc'},$opts->{'msd'});
-  my $wt_samples = Sanger::CGP::Vcf::BamUtil->parse_samples($wt_sam->header->text,$opts->{'wss'},$opts->{'wsq'},$opts->{'wsa'},$opts->{'wsc'},$opts->{'wsd'});
+  my $mt_samples = Sanger::CGP::Vcf::BamUtil->parse_samples($mt_sam->header->text,$opts->{'mss'},$opts->{'msq'},$opts->{'msa'},$opts->{'msc'},$opts->{'msd'},$opts->{'msp'});
+  my $wt_samples = Sanger::CGP::Vcf::BamUtil->parse_samples($wt_sam->header->text,$opts->{'wss'},$opts->{'wsq'},$opts->{'wsa'},$opts->{'wsc'},$opts->{'wsd'},$opts->{'wsp'});
+
+  # close files we're finished with
+  undef $mt_sam;
+  undef $wt_sam;
 
   die "No samples found in normal bam file." if(scalar values %$wt_samples == 0);
   die "Multiple samples found in normal bam file." if(scalar values %$wt_samples > 1);
@@ -71,16 +76,16 @@ use Sanger::CGP::Vcf::VcfProcessLog;
   my ($input_loc,$output_loc,$IN_FH,$OUT_FH);
   try{
     #Open in and out files (OR STDIN/STDOUT depending on what's been given at command line)
-    if($opts->{'f'}){
-      open($IN_FH,'<',$opts->{'f'}) or die("Error opening input file $opts->{f}: $!");
-      $input_loc = $opts->{'f'};
+    if($opts->{'i'}){
+      open($IN_FH,'<',$opts->{'i'});
+      $input_loc = $opts->{'i'};
     }else{
       $IN_FH = \*STDIN;
       $input_loc = 'STDIN';
     }
 
     if($opts->{'o'}){
-      open($OUT_FH, '>', $opts->{'o'}) or die("Error opening output file $opts->{o}: $!");
+      open($OUT_FH, '>', $opts->{'o'});
       $output_loc = $opts->{'o'};
     }else{
       $OUT_FH = \*STDOUT;
@@ -103,10 +108,12 @@ use Sanger::CGP::Vcf::VcfProcessLog;
 
 
     #Iterate through input and create a record for each.
+    my $fai = Bio::DB::Sam::Fai->load($opts->{'r'});
     while(<$IN_FH>){
       my $line = $_;
       chomp($line);
       my ($seg_no,$chr,$start,$end,$wt_cn_tot,$wt_cn_min,$mt_cn_tot,$mt_cn_min) = split(',',$line);
+      $start--; # all symbolic ALTs require preceeding base padding
       die("Unrecognised format in CN segments passed: '$line'.") unless(defined($seg_no)
                                                                       && defined($chr)
                                                                       && defined($start)
@@ -123,8 +130,8 @@ use Sanger::CGP::Vcf::VcfProcessLog;
   }catch{
     die "Error! Reading from: |$input_loc| Writing to: |$output_loc|\n$_";
   }finally{
-    close($IN_FH) or die("Error trying to close $opts->{f}: $!") if($opts->{'f'});
-    close($OUT_FH) or die("Error trying to close $opts->{o}: $!") if($opts->{'o'});
+    close($IN_FH) if($opts->{'i'});
+    close($OUT_FH) if($opts->{'o'});
   }
 
 }
@@ -139,17 +146,17 @@ sub setup{
 					'm|man' => \$opts{'m'},
 					'v|version' => \$opts{'v'},
 					'o|out-file=s' => \$opts{'o'},
-          'f|in-file=s' => \$opts{'f'},
+          'f|in-file=s' => \$opts{'i'},
           'mss|sample-study-mut=s' => \$opts{'mss'},
           'msa|sample-accession-mut=s' => \$opts{'msa'},
           'msc|sample-accession-source-mut=s' => \$opts{'msc'},
-          'msp|sample-platform-mut=s' => \$opts{'msp'},
+          'msp|seq-platform-mut=s' => \$opts{'msp'},
           'msq|sample-sequencing-protocol-mut=s' =>\$opts{'msq'},
           'msd|sample-description-mut=s' => \$opts{'msd'},
           'wss|sample-study-norm=s' => \$opts{'wss'},
           'wsa|sample-accession-norm=s' => \$opts{'wsa'},
           'wsc|sample-accession-source-norm=s' => \$opts{'wsc'},
-          'wsp|sample-platform-norm=s' => \$opts{'wsp'},
+          'wsp|seq-platform-norm=s' => \$opts{'wsp'},
           'wsq|sample-sequencing-protocol-norm=s' =>\$opts{'wsq'},
           'wsd|sample-description-norm=s' => \$opts{'wsd'},
           'sbm|sample-bam-mut=s' => \$opts{'sbm'},
@@ -167,34 +174,22 @@ sub setup{
 
   pod2usage(-verbose => 1) if(defined $opts{'h'});
   pod2usage(-verbose => 2) if(defined $opts{'m'});
-  if($opts{'f'}){
-    if(! -e $opts{'f'}){
-      pod2usage(-message  => "\nERROR: f|in-file must an existing file if defined, otherwise STDIN is used: '".$opts{'f'}."'.\n", -verbose => 1,  -output => \*STDERR);
-    }
+
+
+  if($opts{'i'}){
+    # can come from STDIN if not defined
+    PCAP::Cli::file_for_reading('i', $opts{'i'});
   }
-  pod2usage(-message  => "\nERROR: mss|sample-study-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'mss'});
-  pod2usage(-message  => "\nERROR: msa|sample-accession-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'msa'});
-  pod2usage(-message  => "\nERROR: msc|sample-accession-source-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'msc'});
-  pod2usage(-message  => "\nERROR: msp|sample-platform-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'msp'});
-  pod2usage(-message  => "\nERROR: msq|sample-sequencing-protocol-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'msq'});
-  pod2usage(-message  => "\nERROR: msd|sample-description-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'msd'});
-  pod2usage(-message  => "\nERROR: wss|sample-study-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'wss'});
-  pod2usage(-message  => "\nERROR: wsa|sample-accession-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'wsa'});
-  pod2usage(-message  => "\nERROR: wsc|sample-accession-source-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'wsc'});
-  pod2usage(-message  => "\nERROR: wsp|sample-platform-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'wsp'});
-  pod2usage(-message  => "\nERROR: wsq|sample-sequencing-protocol-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'wsq'});
-  pod2usage(-message  => "\nERROR: wsd|sample-description-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'wsd'});
-  pod2usage(-message  => "\nERROR: sbm|sample-bam-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'sbm'});
-  if(! -e $opts{'sbm'}){
-    pod2usage(-message  => "\nERROR: sbm|sample-bam-mut must be an existing file.\n", -verbose => 1,  -output => \*STDERR);
-  }
-  pod2usage(-message  => "\nERROR: sbw|sample-bam-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'sbw'});
-  if(! -e $opts{'sbw'}){
-    pod2usage(-message  => "\nERROR: sbw|sample-bam-norm must be an existing file.\n", -verbose => 1,  -output => \*STDERR);
-  }
-  pod2usage(-message  => "\nERROR: r|reference must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'r'});
+  PCAP::Cli::file_for_reading('sbm', $opts{'sbm'});
+  PCAP::Cli::file_for_reading('sbw', $opts{'sbw'});
+  PCAP::Cli::file_for_reading('r', $opts{'r'});
+
+  # required: direct input
   pod2usage(-message  => "\nERROR: rs|reference-species must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'rs'});
-  pod2usage(-message  => "\nERROR: ra|reference-assembly must be defined.\n", -verbose => 1,  -output => \*STDERR) unless($opts{'ra'});
+
+  pod2usage(-message  => "\nERROR: msq|sample-sequencing-protocol-mut must be defined.\n", -verbose => 1,  -output => \*STDERR) if(exists $opts{'msq'} && ! defined $opts{'msq'});
+  pod2usage(-message  => "\nERROR: wsq|sample-sequencing-protocol-norm must be defined.\n", -verbose => 1,  -output => \*STDERR) if(exists $opts{'wsq'} && ! defined $opts{'wsq'});
+
   return \%opts;
 }
 
@@ -209,26 +204,26 @@ CN_to_VCF.pl - Take CGP copy number algorithm segment output and convert to VCF 
 
 CN_to_VCF.pl [options]
 
-  	Required parameters:
-     -out-file                        -o   Output file.
-     -in-file                         -f   Input file.
-     -sample-study-mut                -mss  Sample study.
-     -sample-accession-mut            -msa  Sample Accession.
-     -sample-accession-source-mut     -msc  Sample Accession Source.
-     -sample-platform-mut             -msp  Sample Platform.
-     -sample-sequencing-protocol-mut  -msq  Sample Sequencing Protocol.
-     -sample-description-mut          -msd  Sample Description.
-     -sample-study-norm               -wss  Sample study.
-     -sample-accession-norm           -wsa  Sample Accession.
-     -sample-accession-source-norm    -wsc  Sample Accession Source.
-     -sample-platform-norm            -wsp  Sample Platform.
-     -sample-sequencing-protocol-norm -wsq  Sample Sequencing Protocol.
-     -sample-description-norm         -wsd  Sample Description.
-     -sample-bam-mut                  -sbm  Mutant sample bam file.
-     -sample-bam-norm                 -sbw  Normal sample bam file.
-     -reference-species               -rs   Reference species.
-     -reference-assembly              -ra   Reference assembly.
-     -reference                       -r    Reference file
+    Required parameters:
+      -sample-bam-mut                  -sbm  Mutant sample bam file.
+      -sample-bam-norm                 -sbw  Normal sample bam file.
+      -reference                       -r    Reference file
+      -sample-sequencing-protocol-mut  -msq  Sample Sequencing Protocol.
+      -sample-sequencing-protocol-norm -wsq  Sample Sequencing Protocol.
+
+    Optional parameters:
+      -in-file                         -i    Input file. [STDIN]
+      -out-file                        -o    Output file [STDOUT].
+      -reference-species               -rs   Reference species [BAM HEADER].
+      -reference-assembly              -ra   Reference assembly [BAM HEADER].
+      -sample-study-mut                -mss  Mut sample study.
+      -sample-accession-mut            -msa  Mut sample accession [BAM HEADER].
+      -sample-accession-source-mut     -msc  Mut sample accession source.
+      -seq-platform-mut                -msp  Mut sequencing platform. [BAM HEADER]
+      -sample-study-norm               -wss  Normal sample study.
+      -sample-accession-norm           -wsa  Normal sample accession [BAM HEADER].
+      -sample-accession-source-norm    -wsc  Normal sample accession source.
+      -seq-platform-norm               -wsp  Normal sequencing platform [BAM HEADER].
 
     Other:
      -help     -h   Brief help message.
@@ -236,7 +231,7 @@ CN_to_VCF.pl [options]
      -version  -v   Version information.
 
 
-=head1 OPTIONS
+=head1 PARAMETERS
 
 =over 8
 
@@ -335,4 +330,3 @@ Prints the manual page and exits.
 B<CN_to_VCF.pl> will attempt convert the input segmented file into VCF copy number format.
 
 =cut
-

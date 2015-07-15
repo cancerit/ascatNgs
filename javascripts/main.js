47d481e15ccc34ca705bcc7eaeb9927f793ea867
Simon Beal <sb48@sanger.ac.uk>
2015-07-15 08:31:02 +0100

    Update style

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,38 @@
-console.log('This would be the main JS file.');
+var current_project = location.pathname.split("/")[1];
+//var current_project = "cgpPindel"
+
+var github_api_url = 'https://api.github.com/repos/cancerit/'.concat(current_project, '/readme?ref=master');
+
+var corsproxy = "http://crossorigin.me/";
+var github_url = "https://github.com/cancerit/".concat(current_project, "/raw/master/README.md");
+var proxy_url = corsproxy.concat(github_url);
+
+var error_html = "<small>Using fail-over page generation, please notify <a href='mailto:cgp-it@sanger.ac.uk' target='_top'>cgp-it@sanger.ac.uk</a></small><br>";
+
+$.ajax({
+	url:github_api_url,
+	headers: {
+	    "Accept": "application/vnd.github.3.html",
+	    "User-Agent": "CancerIT"
+	}
+    })
+    .success(function (response) {
+	    $('#content').replaceWith(response); 
+	})
+    .error(function (r) {
+	    $.get(proxy_url)
+		.success(function (response) {
+			var converter = new showdown.Converter(),
+			    html = converter.makeHtml(response);
+			$("#content").html(html);
+		    });
+	});
+
+window.onload = function () {
+    $("a.view_github_link" ).attr("href", "https://github.com/cancerit/".concat(current_project));
+    $("a.zip_download_link").attr("href", "https://github.com/cancerit/".concat(current_project, "/zipball/master"));
+    $("a.tar_download_link").attr("href", "https://github.com/cancerit/".concat(current_project, "/tarball/master"));
+    $("h1.header").html(current_project);
+    $("projectname").html(current_project);
+    document.title = current_project;
+};
---
+++ javascripts/showdown/subParsers/headers.js
@@ -0,0 +1,80 @@
+showdown.subParser('headers', function (text, options, globals) {
+  'use strict';
+
+  var prefixHeader = options.prefixHeaderId;
+
+  // Set text-style headers:
+  //	Header 1
+  //	========
+  //
+  //	Header 2
+  //	--------
+  //
+  text = text.replace(/^(.+)[ \t]*\n=+[ \t]*\n+/gm, function (wholeMatch, m1) {
+
+    var spanGamut = showdown.subParser('spanGamut')(m1, options, globals),
+        hID = (options.noHeaderId) ? '' : ' id="' + headerId(m1) + '"',
+        hLevel = parseInt(options.headerLevelStart),
+        hashBlock = '<h' + hLevel + hID + '>' + spanGamut + '</h' + hLevel + '>';
+    return showdown.subParser('hashBlock')(hashBlock, options, globals);
+  });
+
+  text = text.replace(/^(.+)[ \t]*\n-+[ \t]*\n+/gm, function (matchFound, m1) {
+    var spanGamut = showdown.subParser('spanGamut')(m1, options, globals),
+        hID = (options.noHeaderId) ? '' : ' id="' + headerId(m1) + '"',
+        hLevel = parseInt(options.headerLevelStart) + 1,
+      hashBlock = '<h' + hLevel + hID + '>' + spanGamut + '</h' + hLevel + '>';
+    return showdown.subParser('hashBlock')(hashBlock, options, globals);
+  });
+
+  // atx-style headers:
+  //  # Header 1
+  //  ## Header 2
+  //  ## Header 2 with closing hashes ##
+  //  ...
+  //  ###### Header 6
+  //
+
+  /*
+   text = text.replace(/
+   ^(\#{1,6})				// $1 = string of #'s
+   [ \t]*
+   (.+?)					// $2 = Header text
+   [ \t]*
+   \#*						// optional closing #'s (not counted)
+   \n+
+   /gm, function() {...});
+   */
+
+  text = text.replace(/^(#{1,6})[ \t]*(.+?)[ \t]*#*\n+/gm, function (wholeMatch, m1, m2) {
+    var span = showdown.subParser('spanGamut')(m2, options, globals),
+        hID = (options.noHeaderId) ? '' : ' id="' + headerId(m2) + '"',
+        hLevel = parseInt(options.headerLevelStart) - 1 + m1.length,
+        header = '<h' + hLevel + hID + '>' + span + '</h' + hLevel + '>';
+
+    return showdown.subParser('hashBlock')(header, options, globals);
+  });
+
+  function headerId(m) {
+    var title, escapedId = m.replace(/[^\w]/g, '').toLowerCase();
+
+    if (globals.hashLinkCounts[escapedId]) {
+      title = escapedId + '-' + (globals.hashLinkCounts[escapedId]++);
+    } else {
+      title = escapedId;
+      globals.hashLinkCounts[escapedId] = 1;
+    }
+
+    // Prefix id to prevent causing inadvertent pre-existing style matches.
+    if (prefixHeader === true) {
+      prefixHeader = 'section';
+    }
+
+    if (showdown.helper.isString(prefixHeader)) {
+      return prefixHeader + title;
+    }
+    return title;
+  }
+
+  return text;
+});

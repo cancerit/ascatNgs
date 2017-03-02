e8e07bf5e1dbe52678944dada26c7ea754427158
Keiran Raine <kr2@sanger.ac.uk>
2017-03-02 15:49:58 +0000

    Reduce usage od Capture::Tiny as much as possible

--- perl/lib/Sanger/CGP/Ascat.pm
+++ perl/lib/Sanger/CGP/Ascat.pm
@@ -26,12 +26,12 @@ use strict;
 use Const::Fast qw(const);
 use base 'Exporter';
 
-our $VERSION = '4.0.0';
+our $VERSION = '4.0.1';
 our @EXPORT = qw($VERSION);
 
 const my $LICENSE =>
 '##########LICENCE##########
-# Copyright (c) 2014-2016 Genome Research Ltd.
+# Copyright (c) 2014-2017 Genome Research Ltd.
 #
 # Author: CancerIT <cgpit@sanger.ac.uk>
 #

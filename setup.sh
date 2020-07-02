5f76806ca1fad53e516df9fd9d1691a23d9fa691
ra11 <ra11@sanger.ac.uk>
2020-07-02 17:05:07 +0000

    working installation with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-########## LICENCE ##########
-# Copyright (c) 2014-2019 Genome Research Ltd.
+########## LICENSE ##########
+# Copyright (c) 2016-2020 Genome Research Ltd.
 #
-# Author: CASM/Cancer IT <cgphelp@sanger.ac.uk>
+# Author: Cancer Genome Project cgphelp@sanger.ac.uk
 #
-# This file is part of AscatNGS.
+# This file is part of cgpBigWig.
 #
-# AscatNGS is free software: you can redistribute it and/or modify it under
+# cgpBigWig is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License as published by the Free
 # Software Foundation; either version 3 of the License, or (at your option) any
 # later version.
@@ -19,15 +19,24 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-########## LICENCE ##########
+#
+#    1. The usage of a range of years within a copyright statement contained within
+#    this distribution should be interpreted as being equivalent to a list of years
+#    including the first and last year specified and all consecutive years between
+#    them. For example, a copyright statement that reads ‘Copyright (c) 2005, 2007-
+#    2009, 2011-2012’ should be interpreted as being identical to a statement that
+#    reads ‘Copyright (c) 2005, 2007, 2008, 2009, 2011, 2012’ and a copyright
+#    statement that reads ‘Copyright (c) 2005-2012’ should be interpreted as being
+#    identical to a statement that reads ‘Copyright (c) 2005, 2006, 2007, 2008,
+#    2009, 2010, 2011, 2012’."
+#
+###########################
 
-# Update info in README.md if changed
-ASCAT_SRC="https://raw.githubusercontent.com/Crick-CancerGenomics/ascat/v2.5.1/ASCAT/R/ascat.R"
-EXP_ACV="3.3.0"
-
-version_gt () {
-  test $(printf '%s\n' $@ | sort -V | head -n 1) == "$EXP_ACV";
-}
+# ALL tool versions used by opt-build.sh
+# need to keep in sync with Dockerfile
+export VER_CGPVCF="v2.2.1"
+export VER_VCFTOOLS="0.1.16"
+export VER_ALLELECOUNT="v4.0.0"
 
 get_file () {
 # output, source
@@ -38,12 +47,13 @@ get_file () {
   fi
 }
 
+
 if [[ ($# -ne 1 && $# -ne 2) ]] ; then
   echo "Please provide an installation path and optionally perl lib paths to allow, e.g."
   echo "  ./setup.sh /opt/myBundle"
   echo "OR all elements versioned:"
   echo "  ./setup.sh /opt/cgpVcf-X.X.X /opt/PCAP-X.X.X/lib/perl:/some/other/lib/perl..."
-  exit 0
+  exit 1
 fi
 
 INST_PATH=$1
@@ -55,7 +65,7 @@ fi
 # get current directory
 INIT_DIR=`pwd`
 
-set -eu
+set -e
 
 # cleanup inst_path
 mkdir -p $INST_PATH/bin
@@ -82,84 +92,22 @@ export PATH=$INST_PATH/bin:$PATH
 SETUP_DIR=$INIT_DIR/install_tmp
 mkdir -p $SETUP_DIR
 
-cd $SETUP_DIR
-
-## grab cpanm and stick in workspace, then do a self upgrade into bin:
+#install cpanm
 get_file $SETUP_DIR/cpanm https://cpanmin.us/
 perl $SETUP_DIR/cpanm -l $INST_PATH App::cpanminus
-CPANM=`which cpanm`
-echo $CPANM
 
-PCAP=`perl -le 'eval "require $ARGV[0]" and print $ARGV[0]->VERSION' PCAP`
-if [[ "x$PCAP" == "x" ]] ; then
-  echo "PREREQUISITE: Please install PCAP-core before proceeding:"
-  echo "  https://github.com/ICGC-TCGA-PanCancer/PCAP-core/releases"
-  exit 1;
-fi
-
-AC=`perl -le 'eval "require $ARGV[0]" and print $ARGV[0]->VERSION' Sanger::CGP::AlleleCount`
-if [[ "x$AC" == "x" ]] ; then
-  echo "PREREQUISITE: Please install alleleCount version >= $EXP_ACV before proceeding:"
-  echo "  https://github.com/cancerit/alleleCount/releases"
-  exit 1;
-else
-  if version_gt $AC $EXP_ACV; then
-    echo "  alleleCounter version is good ($AC)"
-  else
-    echo "PREREQUISITE: Please install alleleCount version >= $EXP_ACV before proceeding (Found version $AC):"
-    echo "  https://github.com/cancerit/alleleCount/releases"
-    exit 1;
-  fi
-fi
-
-VCF=`perl -le 'eval "require $ARGV[0]" and print $ARGV[0]->VERSION' Sanger::CGP::Vcf`
-if [[ "x$VCF" == "x" ]] ; then
-  echo "PREREQUISITE: Please install cgpVcf before proceeding:"
-  echo "  https://github.com/cancerit/cgpVcf/releases"
-  exit 1;
-fi
-
-HTS=`perl -le 'eval "require $ARGV[0]" and print $ARGV[0]->VERSION' Bio::DB::HTS`
-if [[ "x$HTS" == "x" ]] ; then
-  echo "PREREQUISITE: Please install Bio::DB::HTS before proceeding:"
-  echo "  https://github.com/Ensembl/Bio-HTS/releases"
-  exit 1;
-fi
-
-perlmods=( "File::ShareDir" "File::ShareDir::Install" )
-
-for i in "${perlmods[@]}" ; do
-  echo "Installing build prerequisite $i..."
-  $CPANM -v --mirror http://cpan.metacpan.org -l $INST_PATH $i
-done
-
-cd $INIT_DIR/perl
-
-echo -n "Installing Perl prerequisites ..."
-if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
-    echo
-    echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
-fi
-
-$CPANM -v --mirror http://cpan.metacpan.org --notest -l $INST_PATH/ --installdeps . < /dev/null
-
-echo -n "Installing ascatNgs ..."
-get_file share/ascat/ascat.R $ASCAT_SRC
-perl Makefile.PL INSTALL_BASE=$INST_PATH
-make
-make test
-make install
-rm share/ascat/ascat.R
-
-# cleanup all junk
-rm -rf $SETUP_DIR
+#install R stuff
+export R_LIBS=$INST_PATH/R-libs
+export R_LIBS_USER=$INST_PATH/R-libs
+mkdir -p $R_LIBS_USER
+Rscript build/libInstall.R $R_LIBS_USER 2>&1 | grep '^\*'
+bash build/opt-build.sh $INST_PATH
+bash build/opt-build-local.sh $INST_PATH
 
 echo
 echo
 echo "Please add the following to beginning of path:"
 echo "  $INST_PATH/bin"
-echo "Please add the following to beginning of PERL5LIB:"
-echo "  $PERLROOT"
 echo
 
 exit 0
